3f0ed178365e32ff4e44ebf9fe35d76604bc825d
MADAN KUMAR K <MADAN KUMAR K@LAPTOP-IQ9B34OU>
2022-12-21 22:37:08 +0530

    cmt

---
+++ src/test/java/mock_practice/practice_pro.java
@@ -0,0 +1,16 @@
+package mock_practice;
+
+import org.testng.annotations.Test;
+
+public class practice_pro {
+	@Test
+	public void practice()
+	{
+		System.out.println("practice-1");
+	}
+	public void practice2()
+	{
+		System.out.println("practice-2");
+	}
+
+}
